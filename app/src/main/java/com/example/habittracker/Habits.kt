2a3ef7d41d5ad26857a683b265c1c3c185498b47
Ishu08 <ishu892k1@gmail.com>
2021-04-06 01:08:57 +0530

    Habit Tracker Module 1, 2, 3

--- app/src/main/java/com/example/habittracker/Habits.kt
+++ app/src/main/java/com/example/habittracker/Habits.kt
@@ -1,11 +1,19 @@
 package com.example.habittracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.core.content.ContextCompat.startActivity
 
 class Habits : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_habits)
+
+
+
+
     }
 }
+
