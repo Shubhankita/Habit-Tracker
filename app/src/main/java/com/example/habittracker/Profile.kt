0cd24f914c104c5c681fbf17866cea16073d1569
Ishu08 <ishu892k1@gmail.com>
2021-04-14 22:59:30 +0530

    Habit Tracker Module (1, 2, 3)

--- app/src/main/java/com/example/habittracker/Profile.kt
+++ app/src/main/java/com/example/habittracker/Profile.kt
@@ -1,11 +1,24 @@
 package com.example.habittracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import com.google.firebase.auth.FirebaseAuth
 
 class Profile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        val logout = findViewById<Button>(R.id.logout)
+        logout.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(this@Profile, login::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
     }
 }
