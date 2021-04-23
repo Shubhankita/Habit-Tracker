be70c7119e21b11eebc953713ce9b5885dbd39ec
Shubhankita <shubhankitasood@yahoo.in>
2021-04-23 14:16:23 +0530

    Adding missing Drawables + Starting work on delete habits

--- app/src/main/java/com/example/habittracker/DeleteHabit.kt
+++ app/src/main/java/com/example/habittracker/DeleteHabit.kt
@@ -2,8 +2,10 @@ package com.example.habittracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.habittracker.databinding.ActivityDeleteHabitBinding
 
 class DeleteHabit : AppCompatActivity() {
+    private lateinit var binding: ActivityDeleteHabitBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_delete_habit)
