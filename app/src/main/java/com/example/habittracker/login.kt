e54c3ddb70fc06f9fbecb1718a20a2fdbcc71fcc
Shubhankita <shubhankitasood@yahoo.in>
2021-04-28 16:19:56 +0530

    Google sign in register module

--- app/src/main/java/com/example/habittracker/login.kt
+++ app/src/main/java/com/example/habittracker/login.kt
@@ -1,14 +1,13 @@
 package com.example.habittracker
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import com.google.firebase.auth.GoogleAuthProvider
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
@@ -16,15 +15,14 @@ import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
-import kotlinx.android.synthetic.main.activity_login.tEmailAddress
-import kotlinx.android.synthetic.main.activity_login.tPassword
 
 
 
 class login : AppCompatActivity() {
     companion object {
-        private const val RC_SIGN_IN = 120
+        const val RC_SIGN_IN = 120
     }
 
     private lateinit var mAuth: FirebaseAuth
